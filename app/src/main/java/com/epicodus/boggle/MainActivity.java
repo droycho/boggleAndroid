f089831357e960f307f80709e9dfe4bf2e6efe9b
DJ Roychoudhury and Kevin Deganos <dj+kevin@>
2016-06-28 14:57:26 -0700

    display randomly generated letters in appropriate field.

--- app/src/main/java/com/epicodus/boggle/MainActivity.java
+++ app/src/main/java/com/epicodus/boggle/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -13,6 +14,7 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.rollDiceButton) Button mRollDiceButton;
+    @Bind(R.id.lettersDisplay) TextView mLettersDisplay;
     private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
@@ -21,17 +23,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+
+
         mRollDiceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String alphabet = "abcdefghijklmnopqrstuvwxyz";
                 final String vowels = "aeiou";
+                String letters = "";
 
                 Random r = new Random();
                 for (int i = 0; i < 8; i++) {
                     char letter = alphabet.charAt(r.nextInt(26));
                     String outputLetter = Character.toString(letter);
-                    Log.d(TAG, outputLetter);
+                    letters= letters + letter;
+                    mLettersDisplay.setText(letters);
                 }
             }
         });
